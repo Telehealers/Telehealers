3653c48b91cfdf30a90a58f7d51e2b6988ef9acd
ayushv <ayushvermaiitd@gmail.com>
2021-06-22 01:52:23 +0530

    added tutorial videos

--- application/views/appointment.php
+++ application/views/appointment.php
@@ -460,7 +460,7 @@ input.range::-ms-fill-upper {
     <?php   $attributes = array('class' => 'form-horizontal','role'=>'form');
             echo form_open_multipart('Appointment/confirmation', $attributes);
       ?>
-    <div id="q_succ_msg" class="mt-3 alert alert-success" style="display:none"></div>
+    <div id="q_succ_msg" class="mt-3 alert alert-warning" style="display:none"></div>
 
     <div class="row pt-4" style="margin-bottom: 20px;padding-left:2%;padding-right:2%">
     <div class="col-sm-12 col-md-4 col-lg-4">
@@ -563,7 +563,6 @@ input.range::-ms-fill-upper {
     </div></div>
     
     <div class="row mb-3" id="department_type" style="display:none;padding-left:2%;padding-right:2%">
-
     <div class="col-sm-12 col-md-12 col-lg-12">
     <h1 for="customRange3" class="form-label labelStyle mb-4 mt-4" style="margin-top:0%">Departments</h1>
     <div class="btn-group" id="elem" style="width:inherit;height:40px !important;display: flex;
@@ -601,9 +600,22 @@ input.range::-ms-fill-upper {
         <input type="hidden" name="servicetype_id" id="servicetype_id" >
     </div>
     </div>
-    <div class="container" style="padding:0px;height: 500px;overflow-y: auto;overflow-x: hidden;width: auto;">
-  <div class="row"  id="docs" style="padding-left:2%;padding-right:2%">
+    <div class="embed-responsive border border-primary rounded" id="tutorialPhone" style="display: none;padding-bottom: 150%;">
+      <video class="embed-responsive-item responsive-iframe" loop autoplay muted controls>
+        <source src="../../web_assets/videos/telehealers-book-on-phone-compressed.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+      </video>
+    </div>
+      <div class="embed-responsive embed-responsive-16by9 border border-primary rounded" id="tutorial" style="display: none">
+         <video class="embed-responsive-item responsive-iframe" autoplay muted loop controls>
+        <source src="../../web_assets/videos/book-appointment-tutorial-pc.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+      </video>
+    </div>
 
+    <div class="container" style="padding:0px;height: 500px;overflow-y: auto;overflow-x: hidden;width: auto;">
+    <div class="row"  id="docs" style="padding-left:2%;padding-right:2%">
+      
     </div>
     </div>
     <div class="container-fluid" style="padding-left:2%;padding-right:2%">
@@ -647,6 +659,33 @@ input.range::-ms-fill-upper {
 </div>></form>
 <script>
 var servicetype='';
+ function IsMobile() {
+          var Uagent = navigator.userAgent||navigator.vendor||window.opera;
+            return(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(Uagent)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(Uagent.substr(0,4))); 
+        };
+function showTutorial() {
+
+$('#q_succ_msg').html('No Consultants available at this time. Watch tutorial video below.');
+$('#q_succ_msg').show();
+if (IsMobile()){
+    $('#tutorialPhone').show();
+} 
+else{
+  $('#tutorial').show();
+}
+}
+
+function hideTutorial() {
+
+if (IsMobile()){
+    $('#tutorialPhone').hide();
+} 
+else{
+  $('#tutorial').hide();
+}
+}
+
+
 function getDoctors(language,date,hour,min,am_pm){
     $('#docs')[0].textContent='';
     $('#my-modal').modal({show: 'false'});
@@ -657,7 +696,16 @@ function getDoctors(language,date,hour,min,am_pm){
     data: {servicetype_id:servicetype,preferred_language:language,booking_date:date,booking_hour:hour,booking_minute:min,booking_am_pm:am_pm},
     type: 'POST',
     success: function(response){
-      $('#docs').prepend(response);
+      if(!$.trim(response)){
+        $('#q_succ_msg').html('No Consultants available at this time');
+        $('#q_succ_msg').show();
+        showTutorial();
+        }
+      else{
+        $('#q_succ_msg').hide();
+        hideTutorial();
+        $('#docs').html(response);
+      }
     }
   });
 }
@@ -683,6 +731,7 @@ function fetchTime(date,hour, minute,am_pm,language){
   time=hour+":"+minute+" "+am_pm;
   console.log(selected_dt.toString(),date_today.toString());
   if(selected_dt.getTime()< date_today.getTime()){
+        showTutorial();
         $('#docs')[0].textContent='';
          return; 
   }
@@ -715,6 +764,9 @@ $(document).ready(function(){
 // $('#minute button').click(function(){
 //     console.log('hello');
 // });
+showTutorial();
+
+
   $("#datepicker").datepicker(
 {format: "yyyy-mm-dd",
 startDate: new Date()
